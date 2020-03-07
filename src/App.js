e38d8540ca261407cea3fca614348ab19cd622bb
GAMHnile <georgeamhuweh@gmail.com>
2020-03-07 09:45:03 +0100

    Initial upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,150 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Navigation from './components/navigation/Navigation';
+import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
+import FaceRecognition from './components/facerecognition/FaceRecognition';
+import Logo from './components/logo/Logo';
+import Rank from './components/rank/Rank';
+import Particles from 'react-particles-js';
+import Signin from './components/signin/Signin';
+import Register from './components/register/register';
 
-function App() {
-  return (
+
+const initialState = { 
+        input: '',
+        imageUrl: '',
+        box: {},
+        route: 'signin',
+        isSignedIn: false,
+        user: {
+                id: '',
+                username: '',
+                email: '',
+                entries: 0,
+                joined: ''
+
+          } 
+        }
+
+
+
+const particlesOptions = {
+                particles: {
+                  number: {
+                    value: 70,
+                    density:{
+                      enable: true,
+                      value_area: 600
+                    }
+                  }
+                }
+              }
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = initialState;
+  }
+
+  loadUser = (data)=>{
+    this.setState({user : {
+            id: data.id,
+            username: data.name,
+            email: data.email,
+            entries: data.entries,
+            joined: data.joined
+
+    }})
+  }
+
+  calculateFaceBox =(data)=>{
+    const box = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width)
+    const height = Number(image.height)
+    return {
+      leftCol: (box.left_col * width),
+      rightCol: (width-(box.right_col*width)),
+      topRow: box.top_row*height,
+      bottomRow: (height-(box.bottom_row*height))
+
+    }
+  }
+
+  createBox =(box)=>{
+    this.setState({box: box})
+  }
+
+
+  onInputChange = (event)=>{
+    this.setState({input: event.target.value})
+  } 
+
+  onButtonSubmit =(event)=>{
+    this.setState({imageUrl: this.state.input})
+    //clarifai call on server
+    fetch('https://still-brook-67027.herokuapp.com/imagesurl', 
+        {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({input: this.state.input })
+        })
+    .then(res => res.json())
+    .then(response=>{ 
+        fetch('https://still-brook-67027.herokuapp.com/images', 
+        {
+          method: 'PUT',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(this.state.user)
+        }).then(res => res.json())
+          .then(update =>{
+            this.setState(Object.assign(this.state.user,{entries: update}))
+                }).catch(err => console.log(err))
+
+      
+      this.createBox(this.calculateFaceBox(response))}   
+      ).catch(err => console.log(err))
+  }
+
+  changeRoute = (route)=>{
+
+    if(route === 'home'){
+      this.setState({isSignedIn: true})
+    }else{
+      this.setState(initialState)
+    }
+    this.setState({route: route})
+    
+  }
+
+  render(){
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Particles  className='particles'
+              params={particlesOptions}
+              
+            />
+
+      <Navigation changeRoute={this.changeRoute} isSignedIn={this.state.isSignedIn} />
+      
+      
+      {(this.state.route === 'signin')
+      ?<Signin loadUser={this.loadUser} changeRoute={this.changeRoute} />
+      :(this.state.route ==='home'?
+        <div>
+
+          
+          <Logo />
+          <Rank name={this.state.user.username} entries={this.state.user.entries} />
+          <ImageLinkForm 
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit} />
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+        </div>
+        :<Register loadUser={this.loadUser} changeRoute={this.changeRoute}/>
+        )}
+    </div>)
+  }
 }
 
 export default App;
---
+++ src/components/signin/Signin.js
@@ -0,0 +1,105 @@
+import React, {Component} from 'react';
+
+
+
+class Signin extends Component {
+	constructor(props){
+		super();
+		this.state = {
+			signinEmail: '',
+			signinPassword: ''
+		}
+
+	}
+
+	onEmailChange=(event)=>{
+		this.setState({signinEmail: event.target.value});
+	}
+	onPasswordChange=(event)=>{
+		this.setState({signinPassword: event.target.value});
+	}
+
+	onSigninSubmit=()=>{
+		const {signinEmail, signinPassword} = this.state;
+		
+		if( signinEmail && signinPassword){
+
+			fetch('https://still-brook-67027.herokuapp.com/signin',
+			{
+				method: 'POST',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify({
+					email: signinEmail,
+					password: signinPassword
+				})
+			})
+			.then(response=> response.json())
+			.then(res=>{
+				 if(res.id){
+	          		this.props.loadUser(res);
+					this.props.changeRoute('home');
+				}
+			});
+
+		}
+
+	}
+
+	render(){
+			const {changeRoute} = this.props;
+
+			return (
+				<div>
+					<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center shadow-5">
+						<main className="pa4 black-80">
+						  <div className="measure "  >
+						    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+						      <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+						      <div className="mt3">
+						        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+						        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+						        	type="email" name="email-address"  
+						        	id="email-address"
+						        	onChange={this.onEmailChange} 
+						        	/>
+						      </div>
+						      <div className="mv3">
+						        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+						        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+						        type="password" 
+						        name="password"  
+						        id="password"
+						        onChange={this.onPasswordChange}
+						        />
+						      </div>
+						    </fieldset>
+						    <div className="">
+						      <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+						      type="submit" 
+						      value="Sign in"
+						       onClick={this.onSigninSubmit}
+						      />
+						    </div>
+						    <div className="lh-copy mt3">
+						      <p onClick={()=>changeRoute('register')} className="f6 link dim black db pointer">Register</p>
+						    </div>
+						  </div>
+						</main>
+					</article>
+
+
+
+
+
+				</div>
+		 
+
+
+				)
+	}
+
+			
+}
+
+
+export default Signin;
